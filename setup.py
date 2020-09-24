71c67d9952c6633b97966591185a0b9bb6730dc3
bluepanee <53943505+bluepanee@users.noreply.github.com>
2020-09-24 20:48:20 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dem-generator',
+    version='1.0',
+    packages=['dem_generator'],
+    package_data={'dem_generator': ['fonts/*']},
+    include_package_data=True,
+    url='https://github.com/bluepanee',
+    license='',
+    author='bluepanee',
+    author_email='',
+    description=''
+)
