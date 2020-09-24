a3997b898bbbf65f4c07ddad86dd7b319b7ea621
bluepanee <53943505+bluepanee@users.noreply.github.com>
2020-09-24 18:04:18 +0000

    Add files via upload

---
+++ tests/example.py
@@ -0,0 +1,13 @@
+import dem_generator
+from PIL import Image
+
+img = Image.open("tests/test.jpg")
+dem = dem_generator.Generator()
+
+top_text = "Top text"
+bottom_text = "Bottom text"
+copyright_ = "@welcomeza"
+min_size, max_size = 720, 720
+
+result = dem.create(img, top_text, bottom_text, copyright_, min_size, max_size)
+result.show()
